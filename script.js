f9b7512fab375c186c3bb83fa99d677c9b1fa9ed
Titouan lebocq <lebocq.tit@gmail.com>
2020-03-12 18:49:05 +0100

    start Display mastermind in js

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
 /* Generation of the masterMind */
 console.log('Starting generating the Mastermind.');
 
+/**
+ * functions
+ */
+    /**
+     * event related
+     */
+    function roundOnclick() {
+        console.log(event.target.id + ' clicked');
+        /* color choice */
+    }
+
+    /**
+     * generation
+    */
+function createRow(gameFrame) {
+    console.log('Row Creation ');
+    var row = document.createElement('div');
+    /* setup row */
+    for (let index = 0; index < 4; index++) {
+        var round = createRound();
+        round.id = createID();
+        round.onclick = function () { roundOnclick(event)};
+        row.appendChild(round);
+    }
+    /* add row to top*/
+    row.classList.add('row');
+    if (gameFrame.firstChild == null) gameFrame.appendChild(row);
+    else gameFrame.insertBefore(row, gameFrame.firstChild);
+}
+
+function createRound() {
+    var round = document.createElement('div');
+    round.classList.add('round');
+    return round;
+}
+
+function createID() {
+    if (typeof nbID == 'undefined') nbID = 0;
+    else nbID++;
+    return 'circle' + nbID;
+}
+
+/**
+ * active part
+ */
 let gameFrame = document.getElementById("Game");
 
 gameFrame.style.height = "100vh";
 gameFrame.style.background = "#FDF1B8";
 gameFrame.style.overflow = "hidden";
 
-var content = document.createElement("div");
-var newContent = document.createTextNode('Hi there and greetings!');
-content.appendChild(newContent);
-ocument.body.insertBefore(content, gameFrame);
+createRow(gameFrame);
+createRow(gameFrame);
