013d5d7f1ca625cc2a9cc76b177433fcedcd6b5e
Titouan lebocq <lebocq.tit@gmail.com>
2020-03-15 00:23:27 +0100

    check if all elemnt colors have been choosed

--- script.js
+++ script.js
@@ -1,35 +1,95 @@
 /* Generation of the masterMind */
 console.log('Starting generating the Mastermind.');
 
+var colorList = ['yellow', 'black', 'blue', 'red', 'magenta', 'cyan'];
+let elementChoosed;
+
 /**
  * functions
  */
     /**
      * event related
      */
-    function roundOnclick() {
-        var element = event.target;
+var colorPaletteGenerated = false;
+function roundOnclick() {
+    var element = event.target;
+    var parentClassList = element.parentElement.classList;
+    console.log("this element is inactive : " + parentClassList.contains('inactive'));
+    if (!(parentClassList.contains('inactive'))) {
+        elementChoosed = element;
         console.log(element.id + ' clicked');
         /* color choice */
-        delColorPalette()
         frame = document.getElementById("Game");
-        frame.appendChild(generateColorPalette())
-    }
-
-    function generateColorPalette(element){
-        var palette = document.createElement('div');
-        palette.classList.add('palette');
-        return palette;
-    }
-
-    function delColorPalette() {
         try {
-            var palette = document.getElementById("palette");
-            palette.remove();
+            frame.appendChild(generateColorPalette());
+            setTimeout(function () {
+                colorPaletteGenerated = true;
+            }, 500);
         } catch (error) {
-            
+            console.log("color palette already exist")
         }
     }
+}
+
+function generateColorPalette(element){
+    if (document.getElementById("palette") == null) {
+        var palette = document.createElement('div');
+        palette.id = "palette";
+        colorList.forEach(element => {
+            var colorElement = document.createElement('div');
+            colorElement.classList.add(element);
+            colorElement.classList.add("paletteElement");
+            colorElement.onclick = function () { chooseColor(); };
+            palette.appendChild(colorElement);
+        });
+        return palette;
+    }
+}
+
+function chooseColor() {
+    if (elementChoosed != null) {
+        console.log(event.target.classList[0]);
+        color = event.target.classList[0];
+        elementChoosed.classList.add(color);
+        if (elementChoosed.classList.length > 2) elementChoosed.classList.remove(elementChoosed.classList[1]);
+        delColorPalette();
+    }
+}
+
+function delColorPalette() {
+    try {
+        console.log("destroy palette");
+        var palette = document.getElementById("palette");
+        palette.remove();
+        colorPaletteGenerated = false;
+    } catch (error) {}
+}
+
+function validationCheck(gameFrame) {
+    if (allChecked()) {
+        elementChoosed = null;
+        var rows = document.getElementsByClassName('row');
+        rows[0].classList.add('inactive');
+        delColorPalette();
+        createRow(gameFrame);
+    }
+}
+
+function allChecked() {
+    var activeRow = document.getElementsByClassName("row")[0];
+    var colorsChoosed = 0;
+    (activeRow.childNodes).forEach(elementChild => {
+        (elementChild.classList).forEach(elementChildClass => {
+            colorList.forEach(color => {
+                if (color == elementChildClass) colorsChoosed++;
+            });
+
+            console.log("Element : " + elementChild.id + " | class element :" + elementChildClass + " | actual nb color choosed : " + colorsChoosed);
+        });
+    });
+    console.log("Nb color choosed : " + colorsChoosed);
+    return (colorsChoosed == 4);
+}
 
     /**
      * generation
@@ -71,5 +131,15 @@ gameFrame.style.height = "100vh";
 gameFrame.style.background = "#FDF1B8";
 gameFrame.style.overflow = "hidden";
 
-createRow(gameFrame);
+gameFrame.onclick = function () {
+    console.log("Palette can be deleted : " + colorPaletteGenerated);
+    if (colorPaletteGenerated) delColorPalette();
+}
+
+var b = document.createElement('button');
+b.innerText = "Valider";
+b.id = 'validationButton';
+gameFrame.appendChild(b);
+b.onclick = function () { validationCheck(gameFrame); };
+
 createRow(gameFrame);
