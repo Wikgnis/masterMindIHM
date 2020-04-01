2a2500e2570b54f0f1591a755cc6255266ceb0bd
Titouan lebocq <lebocq.tit@gmail.com>
2020-04-02 00:09:46 +0200

    solid rebase prog

---
+++ MstrMindScript.js
@@ -0,0 +1,195 @@
+/**
+ * Creating elements
+ */
+var nbVis = 0;
+function createIdVisual() {
+    nbVis++;
+    return "Visual_" + nbVis;
+}
+
+function createContainer() {
+    let container = document.createElement("div");
+    container.classList.add("container");
+    for (let i = 0; i < 12; i++) {
+        container.appendChild(createRow());
+    }
+    container.lastChild.classList.remove("inactive");
+    container.lastChild.classList.add("active");
+    return container;
+}
+
+function createRow() {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    row.classList.add("inactive");
+    for (let i = 0; i < 5; i++) {
+        let rowElement = createRowElement();
+        row.appendChild(rowElement);
+        if (i==4) {
+            for (let index = 0; index < 4; index++) {
+                rowElement.appendChild(createHintElement());
+            }
+        }
+        else {
+            rowElement.appendChild(createVisual());
+        }
+    }
+    return row;
+}
+
+function createRowElement() {
+    let rowElement = document.createElement("div");
+    rowElement.classList.add("rowElement")
+    return rowElement;
+}
+
+function createHintElement() {
+    let Hint = document.createElement("div");
+    Hint.classList.add("Hint");
+    return Hint;
+}
+
+function createVisual() {
+    let visual = document.createElement("div");
+    visual.id = createIdVisual();
+    visual.classList.add("visual");
+    visual.onclick = function (event) {
+        let FcdVisual = event.target;
+        Array.from(FcdVisual.parentElement.parentElement.classList).forEach(element => {
+            if ("active" == element) {
+                FocusedVisual = FcdVisual;
+                if (!paletteDeployed) deployPallette();
+                else destroyPalette();
+                paletteDeployed = !paletteDeployed;
+            }
+        });
+        console.log("Focused Visual : " + FocusedVisual + "\n Palette deployed : " + paletteDeployed);
+    }
+    return visual;
+}
+
+function createStyleSheet() {
+    var style = document.createElement("style");
+    style.id = "MstrMind_StyleSheet";
+    style.appendChild(document.createTextNode("")); // WebKit hack
+    style.type = 'text/css';
+    return style;
+}
+
+function styleWrapper() {
+    return newStyle(".MastrMindWrapper", ["width : 100%", "background : red"]);
+}
+
+function newStyle(name, args) {
+    style = new String();
+    style += name + " { ";
+    args.forEach(element => {
+        style += " " + element + ";"
+    });
+    style += "}"
+    return style;
+}
+
+function styleRow() {
+    return newStyle(".row", ["display : flex", "align-items : flex-start"])
+}
+
+function styleRowElements() {
+    let styles = new String();
+    // content
+    let size = 100;
+    styles += newStyle(".rowElement", ["display : flex", "width : " + size + "px", "height : " + size + "px", "background : blue"]);
+    // last child
+    styles += newStyle(".rowElement:last-child", ["width : " + 2 * size + "px", "background : violet", "align-items:center", "justify-content : space-evenly"])
+    // visual
+    styles += newStyle(".visual", ["width : 100%", "height : 100%", "border-radius : 50%", "background : white"])
+    // color choice
+    styles += newStyle(".orange", ["background : #f0932b"]);
+    styles += newStyle(".red", ["background : #eb4d4b"]);
+    styles += newStyle(".green", ["background : #6ab04c"]);
+    styles += newStyle(".white", ["background : #c7ecee"]);
+    styles += newStyle(".blue", ["background : #4834d4"]);
+    styles += newStyle(".black", ["background : #130f40"]);
+    styles += newStyle(".grey", ["background : #535c68"]);
+    styles += newStyle(".violet", ["background : #be2edd"]);
+    return styles;
+}
+
+function styleHint() {
+    let styles = new String();
+    styles += newStyle(".Hint", ["width : 20%", "height : 40%", "border-radius : 50%", "background : white", "display : inline-block"]);
+    styles += newStyle(".found", ["background : green"]);
+    return styles;
+}
+
+function stylePalette() {
+    return newStyle(".palette>div", ["display : inline-block", "width : 60px", "height : 60px"])
+}
+
+function setupStyleSheet(style) {
+    let content = new String();
+    content += styleWrapper();
+    content += styleRowElements();
+    content += stylePalette();
+    content += styleRow();
+    content += styleHint();
+    style.appendChild(document.createTextNode(content));
+}
+
+/**
+ * Editing Elements
+ */
+
+function updateContainer(container) {
+
+}
+
+function updateRow() {
+
+}
+
+function updateRowElement() {
+
+}
+
+var paletteDeployed = false;
+var palette;
+var FocusedVisual = new Object();
+var colors = ["orange", "red", "green", "white", "blue", "black", "grey", "violet"];
+
+function deployPallette() {
+    palette = document.createElement("div");
+    palette.classList.add("palette")
+    colors.forEach(color => {
+        let element = document.createElement("div");
+        element.classList.add(color);
+        element.onclick = function (event) {
+            colors.forEach(color => {
+                FocusedVisual.classList.remove(color);
+            });
+            FocusedVisual.classList.add(event.target.classList);
+        }
+        palette.appendChild(element);
+    });
+    wrappers[0].appendChild(palette);
+    console.log("Palette deployed : " + paletteDeployed);
+}
+
+function destroyPalette() {
+    FocusedVisual = null;
+    palette.parentNode.removeChild(palette);
+    console.log("Palette deployed : " + paletteDeployed);
+}
+
+/**
+ * Main
+ */
+let Style = createStyleSheet();
+
+document.head.appendChild(Style); // Add the <style> element to the page
+setupStyleSheet(Style);
+
+let wrappers = document.getElementsByClassName("MastrMindWrapper");
+Array.from(wrappers).forEach(wrapper =>{
+    wrapper.appendChild(createContainer());
+})
